fab838109d684649efd7d264bae4a16e7733bbb3
Konstantin Yakovlev <64030005+yakovlevkl@users.noreply.github.com>
2021-10-27 00:09:02 +0500

    Added new tests. Made refactoring.

--- QA_Practicum_Project_1/src/main/java/ru/praktikum/scooter/MainPage.java
+++ QA_Practicum_Project_1/src/main/java/ru/praktikum/scooter/MainPage.java
@@ -1,78 +1,67 @@
 package ru.praktikum.scooter;
 
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.SelenideElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class MainPage {
+public class MainPage extends OrderPageStepTwo {
 
     // Button 'Заказать'
     @FindBy(how = How.XPATH,using = "/html/body/div/div/div[1]/div[1]/div[2]/button[1]")
-    private SelenideElement buttonOrder2;
+    private SelenideElement buttonOrderDown;
 
-    // Question: Сколько это стоит? И как оплатить?
-    @FindBy(how = How.CLASS_NAME,using = "accordion__heading")
-    private SelenideElement accordionHeadingQuestion0;
+    // Список вопросов типа -> Question: Сколько это стоит? И как оплатить?
+    @FindBy(how = How.CLASS_NAME,using = "accordion__button")
+    private ElementsCollection accordionHeadingQuestion;
 
-    // Answer: Сутки — 400 рублей. Оплата курьеру — наличными или картой.
+    // Список ответов типа -> Answer: Сутки — 400 рублей. Оплата курьеру — наличными или картой.
     @FindBy(how = How.CLASS_NAME,using = "accordion__panel")
-    private SelenideElement accordionHeadingAnswer0;
+    private ElementsCollection accordionHeadingAnswer;
 
-    // Question: Хочу сразу несколько самокатов! Так можно?
-    // Answer: Пока что у нас так: один заказ — один самокат.
-    // Если хотите покататься с друзьями, можете просто сделать несколько заказов — один за другим.
-    private final String accordionHeadingQuestion1 = "//*[@id=\"accordion__heading-1\"]";
-
-    // Question: Как рассчитывается время аренды?
-    // Answer: Допустим, вы оформляете заказ на 8 мая.
-    //      Мы привозим самокат 8 мая в течение дня.
-    //      Отсчёт времени аренды начинается с момента, когда вы оплатите заказ курьеру.
-    //      Если мы привезли самокат 8 мая в 20:30, суточная аренда закончится 9 мая в 20:30.
-    private final String accordionHeadingQuestion2 = "//*[@id=\"accordion__heading-2\"]";
-
-    // Question: Можно ли заказать самокат прямо на сегодня?
-    // Answer: Только начиная с завтрашнего дня. Но скоро станем расторопнее.
-    private final String accordionHeadingQuestion3 = "//*[@id=\"accordion__heading-3\"]";
-
-    // Question: Можно ли продлить заказ или вернуть самокат раньше?
-    // Answer: Пока что нет!
-    //      Но если что-то срочное — всегда можно позвонить в поддержку по красивому номеру 1010.
-    private final String accordionHeadingQuestion4 = "//*[@id=\"accordion__heading-4\"]";
-
-    // Question: Вы привозите зарядку вместе с самокатом?
-    // Answer: Самокат приезжает к вам с полной зарядкой.
-    //      Этого хватает на восемь суток — даже если будете кататься без передышек и во сне.
-    //      Зарядка не понадобится.
-    private final String accordionHeadingQuestion5 = "//*[@id=\"accordion__heading-5\"]";
-
-    // Question: Можно ли отменить заказ?
-    // Answer: Да, пока самокат не привезли. Штрафа не будет, объяснительной записки тоже не попросим.
-    //      Все же свои.
-    private final String accordionHeadingQuestion6 = "//*[@id=\"accordion__heading-6\"]";
-
-    // Question: Я жизу за МКАДом, привезёте?
-    // Answer: Да, обязательно. Всем самокатов! И Москве, и Московской области.
-    private final String accordionHeadingQuestion7 = "//*[@id=\"accordion__heading-7\"]";
-
-    public void clickOnAccordionHeadingQuestion0() {
-        accordionHeadingQuestion0.scrollTo().click();
+    private String stringForComparison(int number){
+        String stringCompare = null;
+        if (number == 0) {
+            stringCompare = "Сутки — 400 рублей. Оплата курьеру — наличными или картой.";
+        } else if (number == 1) {
+            stringCompare = "Пока что у нас так: один заказ — один самокат. Если хотите покататься с друзьями, можете просто сделать несколько заказов — один за другим.";
+        } else if (number == 2) {
+            stringCompare = "Допустим, вы оформляете заказ на 8 мая. Мы привозим самокат 8 мая в течение дня. Отсчёт времени аренды начинается с момента, когда вы оплатите заказ курьеру. Если мы привезли самокат 8 мая в 20:30, суточная аренда закончится 9 мая в 20:30.";
+        } else if (number == 3) {
+            stringCompare = "Только начиная с завтрашнего дня. Но скоро станем расторопнее.";
+        } else if (number == 4) {
+            stringCompare = "Пока что нет! Но если что-то срочное — всегда можно позвонить в поддержку по красивому номеру 1010.";
+        } else if (number == 5) {
+            stringCompare = "Самокат приезжает к вам с полной зарядкой. Этого хватает на восемь суток — даже если будете кататься без передышек и во сне. Зарядка не понадобится.";
+        } else if (number == 6) {
+            stringCompare = "Да, пока самокат не привезли. Штрафа не будет, объяснительной записки тоже не попросим. Все же свои.";
+        } else if (number == 7) {
+            stringCompare = "Да, обязательно. Всем самокатов! И Москве, и Московской области.";
+        }
+        return stringCompare;
     }
 
-    public void getAccordionHeadingAnswer0() {
-        assertEquals("Сутки — 400 рублей. Оплата курьеру — наличными или картой.",
-                accordionHeadingAnswer0.getText());
+    private void clickOnAccordionHeadingQuestion(int number) {
+        SelenideElement getQuestion = accordionHeadingQuestion.get(number);
+        getQuestion.scrollTo().shouldBe(Condition.visible).click();
     }
 
-    public void clickQuestionsAboutImportantAndGetAnswer() {
+    private void getAccordionHeadingAnswerAndCompare(int number) {
+        SelenideElement getAnswer = accordionHeadingAnswer.get(number);
+        assertEquals(stringForComparison(number), getAnswer.shouldBe(Condition.visible).getText());
+    }
+
+    void clickQuestionsAboutImportantAndGetAnswer(int number) {
         // Выполняем сразу клик по элементу -> получаем строку -> сравниваем с исходной строкой
-        clickOnAccordionHeadingQuestion0();
-        getAccordionHeadingAnswer0();
+        clickOnAccordionHeadingQuestion(number);
+        getAccordionHeadingAnswerAndCompare(number);
     }
 
-    public void clickButtonOrder2() {
+    public void clickButtonOrderDown() {
         // Клик по кнопке Заказа внизу основной страницы
-        buttonOrder2.click();
+        buttonOrderDown.scrollTo().shouldBe(Condition.visible).click();
     }
     
 }
--- QA_Practicum_Project_1/src/test/java/ru/praktikum/scooter/ScooterMainPageTest.java
+++ QA_Practicum_Project_1/src/test/java/ru/praktikum/scooter/ScooterMainPageTest.java
@@ -1,28 +1,83 @@
 package ru.praktikum.scooter;
 
 import org.junit.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 
 import static com.codeborne.selenide.Selenide.open;
 
 public class ScooterMainPageTest {
+    @BeforeEach
+    public void setUp(){
 
-    String pageSite = "https://qa-scooter.praktikum-services.ru/";
-
-    @Test
-    public void listInImportantQuestionsSectionTest() {
-        // Open url
-        MainPage scooterMainPage = open(pageSite, MainPage.class);
-
-        // click on first Important questions and get on first Important Answer
-        scooterMainPage.clickQuestionsAboutImportantAndGetAnswer();
     }
 
     @Test
-    public void orderScooterClickHeaderButtonOrder() {
+    public void listInImportantQuestionsSection() {
         // Open url
-        HeaderPage scooterHeaderPage = open(pageSite, HeaderPage.class);
+        MainPage scooterMainPage =
+                open("https://qa-scooter.praktikum-services.ru/",
+                MainPage.class);
+        scooterMainPage.closeCookies();
+        // click on first Important questions and get on first Important Answer
+        // Argument: Number of question like 0, 1, 2 and etc.
+        for (int i=0; i<=7; i++) {
+            scooterMainPage.clickQuestionsAboutImportantAndGetAnswer(i);
+        }
+    }
+
+    @Test
+    public void fillPageOfOrder() {
+        // Open url
+        OrderPageStepTwo scooterOrderPageStep =
+                open("https://qa-scooter.praktikum-services.ru/",
+                        OrderPageStepTwo.class);
 
         // Click on upper button Order
-        scooterHeaderPage.clickButtonOrder1();
+        scooterOrderPageStep.closeCookies();
+        // Click on upper button Order
+        scooterOrderPageStep.clickHeaderButtonOrder();
+        //fill in the first page of the order
+        scooterOrderPageStep.orderConfirmationStepOne(
+                "Иван",
+                "Иванов",
+                "Ленина,1-55",
+                "Бульвар Рокоссовского",
+                "89222222228");
+
+        //fill in the first page of the order
+        scooterOrderPageStep.orderConfirmationStepTwo(
+                "12.12.2021",
+                "сутки",
+                "black",
+                "Разговаривать без акцента");
+    }
+
+    @Test
+    public void openYandexSite() {
+        // Open url
+        MainPage scooterMainPage =
+                open("https://qa-scooter.praktikum-services.ru/",
+                        MainPage.class);
+
+        scooterMainPage.closeCookies();
+
+        scooterMainPage.clickLinkYandexSite();
+    }
+
+    @Test
+    public void openScooterSite() {
+        // Open url
+        MainPage scooterMainPage =
+                open("https://qa-scooter.praktikum-services.ru/",
+                        MainPage.class);
+
+        scooterMainPage.closeCookies();
+        scooterMainPage.clickButtonOrderDown();
+        scooterMainPage.clickLinkScooterSite();
+    }
+    @AfterEach
+    public void actionsAfterTest() {
+
     }
 }
